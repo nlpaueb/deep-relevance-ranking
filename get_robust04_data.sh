f2c1a3d57c78cf2a54b6f2e280f8183527d9521d
Giorgos Brokos <gbrokos@AUEB243>
2018-09-07 01:20:35 +0300

    Add setup scripts

---
+++ get_robust04_data.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+mkdir -p robust04_data
+
+cd robust04_data
+
+wget -c https://archive.org/download/deep_relevance_ranking_data/robust04_data.tar.gz
+
+tar -zxcf robust04_data.tar.gz
+
