053df867d0a3018de1416140d310314e39d2f929
gbrokos <g.brokos@gmail.com>
2018-09-17 19:24:29 +0300

    Update get_bioasq_data.sh

--- get_bioasq_data.sh
+++ get_bioasq_data.sh
@@ -10,5 +10,8 @@ tar -zxvf bioasq_data.tar.gz
 wget -c https://archive.org/download/biomedical_idf.tar/biomedical_idf.tar.gz
 tar -zxvf biomedical_idf.tar.gz
 
+wget -c https://archive.org/download/pubmed2018_w2v_30D.tar/pubmed2018_w2v_30D.tar.gz
+tar -zxvf pubmed2018_w2v_30D.tar.gz
+
 wget -c https://archive.org/download/pubmed2018_w2v_200D.tar/pubmed2018_w2v_200D.tar.gz
 tar -zxvf pubmed2018_w2v_200D.tar.gz
